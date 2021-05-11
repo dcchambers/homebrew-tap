c93b94cc501ec01c922869064067d1fc8d885958
Dakota Chambers <dakotachambers@gmail.com>
2021-05-11 12:08:42 -0500

    Update notekeeper Formula to 1.1

--- Formula/notekeeper.rb
+++ Formula/notekeeper.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Notekeeper < Formula
   desc "Notekeeper: A tiny bash tool for taking and organizing simple text notes."
   homepage "https://github.com/dcchambers/note-keeper"
-  url "https://github.com/dcchambers/note-keeper/releases/download/v1.0/notekeeper.tar.gz"
-  sha256 "81aa3376f014e25b41030f5070171d9a1b86361f94a2c74ac1989e49a1824866"
-  version "1.0"
+  url "https://github.com/dcchambers/note-keeper/releases/download/v1.1/notekeeper.tar.gz"
+  sha256 "6454637dcc985bbaebae4ac7c0fb35b6b057f796b67439fa89bb6c4d7b4c26e5"
+  version "1.1"
   license "MIT"
 
   def install
