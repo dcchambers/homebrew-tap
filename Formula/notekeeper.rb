7a89c6113c9621dbdd7ede298134545295b65ad7
Dakota Chambers <dakotachambers@gmail.com>
2025-03-27 22:33:41 -0500

    Bump notekeeper to 1.3

--- Formula/notekeeper.rb
+++ Formula/notekeeper.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Notekeeper < Formula
   desc "Notekeeper: A tiny bash tool for taking and organizing simple text notes."
   homepage "https://github.com/dcchambers/note-keeper"
-  url "https://github.com/dcchambers/note-keeper/releases/download/v1.2/notekeeper.tar.gz"
-  sha256 "5d2006b904d7bfae573019111d9f417087cd7956ce6f98353b5ceee1ad7344cb"
-  version "1.2"
+  url "https://github.com/dcchambers/note-keeper/releases/download/v1.3/notekeeper-1.3.tar.gz"
+  sha256 "7d5111c90c0c565f7608f9075cd7ebe51bd987eb388a2ea2cd8156cfa8b03571"
+  version "1.3"
   license "MIT"
 
   def install
